b4d2fbbaff0c97e2f15f6dc562535e7f846464d6
Gabriel Alfaro <gabriel.alfaro.cruz@gmail.com>
2024-05-22 17:41:34 -0600

    use cliente

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,9 +1,17 @@
-import React from "react";
+import { Metadata } from 'next';
+import { CartCounter } from '../../shopping-cart/components/CartCounter';
+
+export const metadata: Metadata = {
+  title:'Shopping Cart',
+  description: 'SEO Counter Page',
+};
 
 export default function CounterPage() {
+
   return (
-    <div>
-      <h1>Hello Counter</h1>
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Productos en el carrito</span>
+      <CartCounter value={20} />
     </div>
   );
 }
